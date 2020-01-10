cfac3e85efbe3c646bd4a5cbd06c849fc7407709
millord <juancarlosmillord@gmail.com>
2020-01-09 22:48:18 -0400

    added the algorithm number 15

--- 14-arrayConversion.ts
+++ 14-arrayConversion.ts
@@ -1,3 +1,27 @@
-function arrayConversion(inputArray: number[]): number {}
+function arrayConversion(inputArray: number[]): number {
+  let isOdd = true
+
+  while(inputArray.length !== 1){
+    inputArray = sumProduct(inputArray, isOdd)
+    isOdd = !isOdd
+  }
+
+  return inputArray[0]
+
+  function sumProduct(nums: number[], isOdd: boolean): number[{
+    const sumProduct: number[] = []
+  
+    if(isOdd){
+      for(let i = 0; i < nums.length; i += 2+){
+        sumProduct.push(nums[i] + nums[i + 1])
+      }
+    } else{
+      for(let i = 0; i  < nums.length; i++){
+       
+      }
+    
+    }
+  }
+}
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
